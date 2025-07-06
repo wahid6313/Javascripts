db512f6e914a40d079375d3538d92c0a6b644dd8
Wahid Ali <wahidali8566@gmail.com>
2025-07-07 02:24:19 +0530

    js

--- Advnace-Dom/script.js
+++ Advnace-Dom/script.js
@@ -1,31 +1,114 @@
-'use strict';
+"use strict";
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener("click", openModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
+
+///////////////////////////////////////////////////
+// SCROLLING SMOOTH-----
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coord = section1.getBoundingClientRect();
+
+  console.log(s1coord);
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // window.scrollTo(
+  //   s1coord.left + window.pageXOffset,
+  //   s1coord.top + window.pageYOffset
+  // );
+
+  window.scrollTo({
+    left: s1coord.left + window.pageXOffset,
+    top: s1coord.top + window.pageYOffset,
+    behavior: "smooth",
+  });
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+///////////////////////////////////////////////////
+// NAVIGATIONS -----
+
+document.querySelectorAll(".nav__link").forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("click");
+  });
+});
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+
+const header = document.querySelector(".header");
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML =
+  "create elemnt through js <button class='btn btn-close-cookie'> got it !</button>";
+
+// header.prepend(message);
+
+header.append(message);
+// header.append(message.cloneNode(true));
+
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+const logo = document.querySelector(".nav__logo");
+console.log(logo.dataset.versionNumber);
+
+const h1 = document.querySelector("h1");
+
+const alert1 = function () {
+  alert("welcome my webpage");
+};
+
+h1.addEventListener("mouseenter", alert1);
+
+setTimeout(() => h1.removeEventListener("mouseenter", alert1), 3000);
