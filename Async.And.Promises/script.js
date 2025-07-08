912cc1986bce19941aefe19178408cb8bfd69cb2
Wahid Ali <wahidali8566@gmail.com>
2025-07-09 00:13:18 +0530

    js

--- Async.And.Promises/script.js
+++ Async.And.Promises/script.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
@@ -10,3 +10,22 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+//////// NAMASTEY JAVASCRIPTS /////////
+
+/**
+ * What is async?
+ * What is await ?
+ * How async await works behind the scenes?
+ * Examples of using async/await
+ * Error Handling
+ * Interviews
+ * Async await vs Promise.then/.catch
+ */
+
+// always return a promise
+async function getData() {
+  return "namastey";
+}
+
+const data = getData();
+console.log(data);
--- SetTimeout/script.js
+++ SetTimeout/script.js
@@ -1,4 +1,35 @@
-setTimeout(() => {
-  console.log("Hi there, i am setTimeout func");
-}, 2000);
-clearTimeout(4);
+// console.log("wahid ali");
+
+// setTimeout(function cbt() {
+//   console.log("siddu");
+// }, 0);
+
+// // fetch(
+// //   "https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg"
+// // ).then(function cbf() {
+// //   console.log("ali");
+// // });
+
+// console.log("hello");
+
+// let startDate = new Date().getTime();
+// let endDate = startDate;
+// while (endDate < startDate + 6000) {
+//   endDate = new Date().getTime();
+// }
+// console.log("while expires");
+
+//////////////////////////////////////
+
+console.log("hello");
+
+function cb() {
+  console.log("callback");
+}
+cb();
+
+setTimeout(function cb() {
+  console.log("callback 2");
+}, 0);
+
+console.log("world");
